cf518ffed42da5c9118af9bb4246067e5dffa9f8
wolfsack <wolfsack22@gmail.com>
2021-11-29 15:09:09 +0100

    Update temperature.py

--- app/temperature.py
+++ app/temperature.py
@@ -1,10 +1,15 @@
 import os
 
+# get file to read
 TEMPERATURE_FILE = os.environ['ROOT_FS'] + "/sys/class/hwmon/hwmon0/temp1_input"
 
-
+# function to get temperature as float
 def get_temp() -> float :
+    # open file in read mode
     with open(TEMPERATURE_FILE, 'r') as f:
-        tmp_string =  f.read()
+        # read file and cast to int
+        tmp_string = f.read()
         tmp_int = int(tmp_string)
+
+        # return as float
         return tmp_int / 1000
